4fe8d72e023cdb5b8c4169257b9ed4ea728e1e65
Lydia Sevelt <LydiaSevelt@gmail.com>
2016-01-22 14:09:30 -0500

    Adding respect for qstat_reduce, running old code if not yes, otherwise pulling from cached xml file. Also included the display of which project a job is assigned to.

--- qstat_user.php
+++ qstat_user.php
@@ -9,22 +9,35 @@
 
  
 </head>
+
 <?php
-$password_length = 20;
-function make_seed() {
-  list($usec, $sec) = explode(' ', microtime());
-  return (float) $sec + ((float) $usec * 100000);
+$owner  = $_GET['owner'];
+$jobstat  = $_GET['jobstat'];
+$queue  = $_GET['queue'];
+echo "<body><table align=center width=95% border=\"1\" cellpadding=\"0\" cellspacing=\"0\"><tbody>";
+include("header.php");
+
+
+if ($qstat_reduce != "yes" ) {
+
+	$password_length = 20;
+	function make_seed() {
+	  list($usec, $sec) = explode(' ', microtime());
+	  return (float) $sec + ((float) $usec * 100000);
+	}
+
+	srand(make_seed());
+
+	$alfa = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+	$token = "";
+	for($i = 0; $i < $password_length; $i ++) {
+	  $token .= $alfa[rand(0, strlen($alfa))];
+	}
 }
 
-srand(make_seed());
-
-$alfa = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-$token = "";
-for($i = 0; $i < $password_length; $i ++) {
-  $token .= $alfa[rand(0, strlen($alfa))];
-}
-
-function show_run($tokenFile,$owner) {
+function show_run($qstat,$owner,$queue) {
+  global $qstat_reduce;
+  global $token;
   echo "<table align=center width=95%xml border=\"1\" cellpadding=\"0\" cellspacing=\"0\">
 	  <tbody>
 		  <tr>
@@ -35,33 +48,47 @@ function show_run($tokenFile,$owner) {
 		  <td>Priority</td>
 		  <td>Name</td>
 		  <td>State</td>
+		  <td>Project </td>
 		  <td>Queue </td>
 		  <td>Start Time</td>
 		  <td>PE</td>
 		  <td>Slots</td>
 		  </tr>";
   
-  $qstat = simplexml_load_file($tokenFile);
-  foreach ($qstat->xpath('//job_list') as $job_list) {
-  $pe=$job_list->requested_pe['name'];
-  echo "          <tr>
-		  <td><a href=qstat_job.php?jobid=$job_list->JB_job_number&owner=$owner>$job_list->JB_job_number</a></td>
-		  <td><a href=qstat_user.php?owner=$job_list->JB_owner>$job_list->JB_owner</a></td>
-		  <td>$job_list->JAT_prio</td>
-		  <td>$job_list->JB_name</td>
-		  <td>$job_list->state</td>
-		  <td><a href=qstat_user.php?queue=$job_list->queue_name&owner=$owner>$job_list->queue_name</a></td>
-		  <td>$job_list->JAT_start_time</td>
-		  <td>$pe</td>
-		  <td>$job_list->slots</td>
-		  </tr>";
+  if ($qstat_reduce != "yes" ) {
+  	$qstat = simplexml_load_file("/tmp/$token.xml");
   }
-  exec("rm $tokenFile");
+  foreach ($qstat->xpath('//job_list') as $job_list) {
+	  if ($job_list->state != 'r') {
+	    continue;
+	  }
+	  if ($owner != "all" && $job_list->JB_owner != $owner) {
+	    continue;
+	  }
+	  if ($queue != "" && $job_list->queue_name != $queue) {
+	    continue;
+	  }
+	  $pe=$job_list->requested_pe['name'];
+	  echo "          <tr>
+			  <td><a href=qstat_job.php?jobid=$job_list->JB_job_number&owner=$owner>$job_list->JB_job_number</a></td>
+			  <td><a href=qstat_user.php?owner=$job_list->JB_owner>$job_list->JB_owner</a></td>
+			  <td>$job_list->JAT_prio</td>
+			  <td>$job_list->JB_name</td>
+			  <td>$job_list->state</td>
+			  <td>$job_list->JB_project</td>
+			  <td><a href=qstat_user.php?queue=$job_list->queue_name&owner=$owner>$job_list->queue_name</a></td>
+			  <td>$job_list->JAT_start_time</td>
+			  <td>$pe</td>
+			  <td>$job_list->slots</td>
+			  </tr>";
+  }
   echo "</tbody></table><br><br>";
 
 }
 
-function show_pend($tokenFile,$owner) {
+function show_pend($qstat,$owner,$queue) {
+  global $qstat_reduce;
+  global $token;
   echo "<table align=center width=95%xml border=\"1\" cellpadding=\"0\" cellspacing=\"0\">
 	  <tbody>
 		  <tr>
@@ -72,37 +99,44 @@ function show_pend($tokenFile,$owner) {
 		  <td>Priority</td>
 		  <td>Name</td>
 		  <td>State</td>
+		  <td>Project </td>
 		  <td>Queue </td>
 		  <td>Submission Time</td>
 		  <td>PE</td>
 		  <td>Slots</td>
 		  </tr>";
-  
-  $qstat = simplexml_load_file($tokenFile);
-  foreach ($qstat->xpath('//job_list') as $job_list) {
-  $pe=$job_list->requested_pe['name'];
-  echo "          <tr>
-		  <td><a href=qstat_job.php?jobid=$job_list->JB_job_number&owner=$owner>$job_list->JB_job_number</a></td>
-		  <td><a href=qstat_user.php?owner=$job_list->JB_owner>$job_list->JB_owner</a></td>
-		  <td>$job_list->JAT_prio</td>
-		  <td>$job_list->JB_name</td>
-		  <td>$job_list->state</td>
-		  <td><a href=qstat_user.php?queue=$job_list->queue_name&owner=$owner>$job_list->queue_name</a></td>
-		  <td>$job_list->JB_submission_time</td>
-		  <td>$pe</td>
-		  <td>$job_list->slots</td>
-		  </tr>";
+  if ($qstat_reduce != "yes" ) {
+  	$qstat = simplexml_load_file("/tmp/$token.xml");
   }
-  exec("rm $tokenFile");
+  foreach ($qstat->xpath('//job_list') as $job_list) {
+	  if ($job_list->state != 'qw') {
+	    continue;
+	  }
+	  if ($owner != "all" && $job_list->JB_owner != $owner) {
+	    continue;
+	  }
+	  if ($queue != "" && $job_list->queue_name != $queue) {
+	    continue;
+	  }
+	  $pe=$job_list->requested_pe['name'];
+	  echo "          <tr>
+			  <td><a href=qstat_job.php?jobid=$job_list->JB_job_number&owner=$owner>$job_list->JB_job_number</a></td>
+			  <td><a href=qstat_user.php?owner=$job_list->JB_owner>$job_list->JB_owner</a></td>
+			  <td>$job_list->JAT_prio</td>
+			  <td>$job_list->JB_name</td>
+			  <td>$job_list->state</td>
+			  <td>$job_list->JB_project</td>
+			  <td><a href=qstat_user.php?queue=$job_list->queue_name&owner=$owner>$job_list->queue_name</a></td>
+			  <td>$job_list->JB_submission_time</td>
+			  <td>$pe</td>
+			  <td>$job_list->slots</td>
+			  </tr>";
+  }
   echo "</tbody></table><br>";
 
 }
 
 
-$owner  = $_GET['owner'];
-$jobstat  = $_GET['jobstat'];
-$queue  = $_GET['queue'];
-echo "<body><table align=center width=95% border=\"1\" cellpadding=\"0\" cellspacing=\"0\"><tbody>";
 echo "<tr><td><h1>PHPQstat</h1></td></tr>
       <tr><td CLASS=\"bottom\" align=center><a href='index.php'>Home</a> *  <a href=\"qhost.php?owner=$owner\">Hosts status</a> *  <a href=\"qstat.php?owner=$owner\">Queue status</a> * <a href=\"qstat_user.php?owner=$owner\">Jobs status ($owner)</a> * <a href=\"about.php?owner=$owner\">About PHPQstat</a></td></tr><tr><td><br>";
 
@@ -110,24 +144,49 @@ if($queue){$queueflag="-q $queue";}else{$queueflag="";}
 
 if($jobstat){$jobstatflag="-s $jobstat";}else{$jobstatflag="";}
 
+if ($qstat_reduce == "yes" ) {
+	$qstat = simplexml_load_file("/tmp/qstat_all.xml");
+}
+
 switch ($jobstat) {
     case "r":
         $jobstatflag="-s r";
-        $out = exec("./gexml -u $owner $jobstatflag $queueflag -o /tmp/$token.xml");   
-        show_run("/tmp/$token.xml",$owner);
+	if ($qstat_reduce != "yes" ) {
+        	$out = exec("./gexml -u $owner $jobstatflag $queueflag -o /tmp/$token.xml");   
+        	show_run("",$owner,$queue);
+		exec("rm /tmp/$token.xml");
+	} else {
+        	show_run($qstat,$owner,$queue);
+	}
         break;
     case "p":
         $jobstatflag="-s p";
-        $out = exec("./gexml -u $owner $jobstatflag $queueflag -o /tmp/$token.xml");
-        show_pend("/tmp/$token.xml",$owner);
+        if ($qstat_reduce != "yes" ) {
+	        $out = exec("./gexml -u $owner $jobstatflag $queueflag -o /tmp/$token.xml");
+	        show_pend("",$owner,$queue);
+		exec("rm /tmp/$token.xml");
+	} else {
+        	show_pend($qstat,$owner,$queue);
+	}
         break;
     default:
         $jobstatflag="-s r";
-        $out = exec("./gexml -u $owner $jobstatflag $queueflag -o /tmp/$token.xml");
-        show_run("/tmp/$token.xml",$owner);
+	if ($qstat_reduce != "yes" ) {
+	        $out = exec("./gexml -u $owner $jobstatflag $queueflag -o /tmp/$token.xml");
+	        show_run("",$owner,$queue);
+		exec("rm /tmp/$token.xml");
+	} else {
+	        show_run($qstat,$owner,$queue);
+	}
+
         $jobstatflag="-s p";
-        $out = exec("./gexml -u $owner $jobstatflag $queueflag -o /tmp/$token.xml");
-        show_pend("/tmp/$token.xml",$owner);
+	if ($qstat_reduce != "yes" ) {
+	        $out = exec("./gexml -u $owner $jobstatflag $queueflag -o /tmp/$token.xml");
+	        show_pend("",$owner,$queue);
+		exec("rm /tmp/$token.xml");
+	} else {
+        	show_pend($qstat,$owner,$queue);
+	}
         break;
 }
 
