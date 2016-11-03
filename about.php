4eab109bdc10c34c1b7618e5931134550ce175e8
Lydia Sevelt <LydiaSevelt@gmail.com>
2016-11-03 18:12:13 -0400

    Updated about. Needs more updating, version bump, rewording, etc.

--- about.php
+++ about.php
@@ -5,14 +5,22 @@
   <meta name="AUTHOR" content="Jordi Blasco Pallares ">
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="KEYWORDS" content="gridengine sge sun hpc supercomputing batch queue linux xml qstat qhost jordi blasco solnu">
-  <link rel="stylesheet" href="phpqstat.css" type="text/css" /> 
+  <link rel="stylesheet" type="text/css" href="jquery-ui.min.css"/>
+  <link rel="stylesheet" type="text/css" href="datatables.min.css"/> 
 </head>
 
 <?php
 $owner  = $_GET['owner'];
-echo "<body><table align=center width=95% border=\"1\" cellpadding=\"0\" cellspacing=\"0\"><tbody>";
-echo "<tr><td><h1>PHPQstat</h1></td></tr>
-      <tr><td CLASS=\"bottom\" align=center><a href='index.php'>Home</a> *  <a href=\"qhost.php?owner=$owner\">Hosts status</a> *  <a href=\"qstat.php?owner=$owner\">Queue status</a> * <a href=\"qstat_user.php?owner=$owner\">Jobs status ($owner)</a> * <a href=\"about.php?owner=$owner\">About PHPQstat</a></td></tr>";
+echo "<body><table align=center width=95% border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tbody>";
+include("header.php");
+echo "<tr><td align=center>
+<a class='ui-button ui-widget ui-corner-all' href=\"index.php\">Home</a> 
+<a class='ui-button ui-widget ui-corner-all' href=\"qhost.php?owner=$owner\">Hosts status</a>
+<a class='ui-button ui-widget ui-corner-all' href=\"qstat.php?owner=$owner\">Queue status</a>
+<a class='ui-button ui-widget ui-corner-all' href=\"qstat_user.php?owner=$owner\">Jobs status ($owner)</a>
+<a class='ui-button ui-widget ui-corner-all' href=\"about.php?owner=$owner\">About PHPQstat</a>
+</td></tr>";
+
 ?>
 
 
@@ -23,11 +31,11 @@ echo "<tr><td><h1>PHPQstat</h1></td></tr>
 <table align=center width=50% border="0" cellpadding="0" cellspacing="0">
 <tr><td align=center>
 <b>PHPQstat</b> is a web interface that allows to connect to the usefull commands of the Grid Engine (GE) batch queue system. With this interface, you can monitor your job status and your queues health at real time.<br><br>
-<b>AUTHOR</b> Written by Jordi Blasco Pallarès.<br>
-<b>REPORTING BUGS</b> Report bugs to <a href=http://sourceforge.net/tracker/?group_id=306687 target=bugs>SourceForge Tracker</a><br>
-<b>LICENSE</b> This is free software: you are free to change and redistribute it. GNU General Public License version 3.0 (<a href=http://gnu.org/licenses/gpl.html target=gpl>GPLv3</a>).<br>
-<b>Version : 0.2.3 (Janurary 2016)</b><br><br>
-<a href=http://phpqstat.sourceforge.net target=SF>http://phpqstat.sourceforge.net</a><br>
+<b>AUTHORS</b> Originally written by Jordi Blasco Pallarès.<br>
+UGE supprt and additional functionallity and added by Lydia Sevelt<br><br>
+<b>LICENSE</b> This is free software: you are free to change and redistribute it. GNU General Public License version 3.0 (<a href=http://gnu.org/licenses/gpl.html target=gpl>GPLv3</a>).<br><br>
+<b>Version : 0.2.5 (September 2016)</b><br>
+<a href=https://github.com/LydiaSevelt/PHPQstat>https://github.com/LydiaSevelt/PHPQstat</a><br>
 </td></tr>
 </table>
 </center>
