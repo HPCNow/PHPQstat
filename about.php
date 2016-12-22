4b09dc95d11e82adef099e9c528bb6060dd3dc9a
Lydia Sevelt <LydiaSevelt@gmail.com>
2016-12-22 18:01:21 -0500

    Adding reference to datatables and jquery to about

--- about.php
+++ about.php
@@ -35,7 +35,10 @@ With this interface, you and your users can monitor your jobs and your queue sta
 <b>AUTHORS</b><br>
 HTML5 interface, UGE support and additional features added by Lydia Sevelt<br>
 Originally written by Jordi Blasco Pallarès.<br><br>
-<b>LICENSE</b><br>This is free software: you are free to change and redistribute it. GNU General Public License version 3.0 (<a href=http://gnu.org/licenses/gpl.html target=gpl>GPLv3</a>).<br><br>
+<b>Additional libraries</b><br>
+HTML5 interface utilizes the excellent <a href=https://datatables.net>datatables</a> and <a href=https://jquery.com>jquery</a> javascript libraries.<br><br>
+<b>LICENSE</b><br>
+This is free software: you are free to change and redistribute it. GNU General Public License version 3.0 (<a href=http://gnu.org/licenses/gpl.html target=gpl>GPLv3</a>).<br><br>
 <b>Version : 0.3.0 (December 2016)</b><br>
 <a href=https://github.com/LydiaSevelt/PHPQstat>https://github.com/LydiaSevelt/PHPQstat</a><br>
 </td></tr>
